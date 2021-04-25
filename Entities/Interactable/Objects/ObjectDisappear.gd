678f80886382b4e5b6f5994a249f71186145e6f6
vandomej <jacob.vandome15@gmail.com>
2021-04-24 17:49:20 -0700

    changing interactables to kinematic bodies

--- Entities/Interactable/Objects/ObjectDisappear.gd
+++ Entities/Interactable/Objects/ObjectDisappear.gd
@@ -2,7 +2,8 @@ extends Spatial
 
 export(AudioStreamSample) var sample
 
-func _on_StaticBody_object_interacted():
+
+func _on_KinematicBody_object_interacted():
 	var radios = get_tree().get_nodes_in_group("radio")
 	if radios:
 		radios[0].pending_signal(sample)

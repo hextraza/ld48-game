960a27c22e3c3de31338d5c64d28d2560b610dd3
hextraza <sebastiancbenjamin@gmail.com>
2021-04-26 15:01:37 -0700

    Admin room sequencing done

--- Entities/Key_1/Key_1.gd
+++ Entities/Key_1/Key_1.gd
@@ -1,6 +1,8 @@
 extends Spatial
 
+export(AudioStreamSample) var radio_sample
 onready var audio := $AudioStreamPlayer
+onready var teddy = get_tree().get_root().get_node("World/DamHall/Admin Room/Teddy/KinematicBody")
 
 func _on_StaticBody_object_interacted():
 	var players = get_tree().get_nodes_in_group("player")
@@ -17,4 +19,13 @@ func _on_StaticBody_object_interacted():
 
 
 func _on_AudioStreamPlayer_finished():
+	yield(get_tree().create_timer(4), "timeout")
+	
+	var radios = get_tree().get_nodes_in_group("radio")
+	if radios:
+		radios[0].pending_signal(radio_sample)
+		yield(radios[0], "radio_finished")
+		
+	teddy.active = true
+	
 	self.queue_free()
